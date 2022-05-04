247250758e73fa6e5013e93e3e46ab8ae94aa888
Restart <45609733+RestartFU@users.noreply.github.com>
2022-05-04 18:58:56 -0400

    zoom

--- go.mod
+++ go.mod
@@ -6,5 +6,6 @@ require (
 	github.com/TheTitanrain/w32 v0.0.0-20200114052255-2654d97dbd3d
 	github.com/fatih/color v1.13.0
 	github.com/kbinani/win v0.3.0
+	github.com/kindlyfire/go-keylogger v0.0.0-20180814110756-f2fda1034e3a
 	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c
 )
--- main.go
+++ main.go
@@ -2,15 +2,44 @@ package main
 
 import (
 	"fmt"
+	"github.com/TheTitanrain/w32"
 	"github.com/fatih/color"
 	"github.com/kbinani/win"
 	"github.com/restartfu/emp/cheat"
 	"github.com/restartfu/emp/emp"
+	"syscall"
+	"unsafe"
 )
 
 func main() {
 	h := emp.New()
-	cheat.Register(cheat.New("Reach", "Hurt entities from further away.", h, 3, cheat.NewReach(win.LPVOID(h.GameID()+0x440C8E0))))
+	for _, c := range []*cheat.Cheat{
+		cheat.New("Reach", "Hurt entities from further away.", h, 3, cheat.NewReach(win.LPVOID(h.GameID()+0x440C8E0))),
+	} {
+		cheat.Register(c)
+	}
+
+	var actualFov float32
+	readProcessMemory(w32.HANDLE(h.Handle()), 0x1809E9EF098, uintptr(unsafe.Pointer(&actualFov)), unsafe.Sizeof(actualFov))
+	zoom := cheat.NewZoom(0x1809E9EF098)
+
+	go func() {
+		var pressed bool
+		for {
+			keyState := w32.GetAsyncKeyState(70)
+			if keyState&(1<<15) != 0 {
+				pressed = true
+			} else {
+				pressed = false
+			}
+			if !pressed {
+				zoom.SetValue(actualFov)
+			} else {
+				zoom.SetValue(10)
+			}
+			zoom.Update(h)
+		}
+	}()
 
 	go func() {
 		<-h.Close
@@ -18,6 +47,8 @@ func main() {
 			c.SetValue(c.DefaultValue())
 			c.Update()
 		}
+		zoom.SetValue(actualFov)
+		zoom.Update(h)
 	}()
 	fmt.Println(color.HiCyanString(`
 Type help to see the list of available cheats, and type exit to gracefully exit the program.
@@ -64,4 +95,22 @@ func scanFloat32() float32 {
 		_, err = fmt.Scan(&v)
 	}
 	return v
+}
+
+var (
+	modkernel32           = syscall.NewLazyDLL("kernel32.dll")
+	procReadProcessMemory = modkernel32.NewProc("ReadProcessMemory")
+)
+
+func readProcessMemory(hProcess w32.HANDLE, lpBaseAddress, lpBuffer, nSize uintptr) (lpNumberOfBytesRead int, ok bool) {
+	var nBytesRead int
+	ret, _, _ := procReadProcessMemory.Call(
+		uintptr(hProcess),
+		lpBaseAddress,
+		lpBuffer,
+		nSize,
+		uintptr(unsafe.Pointer(&nBytesRead)),
+	)
+
+	return nBytesRead, ret != 0
 }
